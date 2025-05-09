ef252fdd8233c079a751772d660a7a4962373672
Liz Melcher <lizmelchercodingbootcamp@gmail.com>
2025-05-08 19:21:55 -0500

    quotes

--- src/components/Advertisement.tsx
+++ src/components/Advertisement.tsx
@@ -6,7 +6,7 @@ interface AdvertisementProps {
 
 function Advertisement(props: AdvertisementProps) {
   return (
-    <div className="advertisement">
+    <div className='advertisement'>
       <p>{props.companyName}</p>
       <p>{props.productDescription}</p>
       <p>{props.price}</p>
